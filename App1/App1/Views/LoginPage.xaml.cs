90aca683a0b7deb3916556d7a8d98ca5e0684374
Usuario <Usuario@DESKTOP-2ITVHDD>
2023-02-24 14:18:18 -0600

    Agregar archivos de proyecto.

---
+++ App1/App1/Views/LoginPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using App1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (txtUsername.Text== "admin" && txtPass.Text == "123")
+            {
+                Navigation.PushAsync(new HomrPage());
+            }
+            else
+            {
+                DisplayAlert("INCORRECTO!!!", "User o Pass incorrectos", "Ok");
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterName());
+        }
+    }
+}
